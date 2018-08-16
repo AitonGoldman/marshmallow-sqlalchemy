7c88a14c53d58cf409d5ebedc093a053665845bc
Aiton Goldman <agoldma@us.ibm.com>
2018-08-15 20:54:34 -0400

    debug

--- marshmallow_sqlalchemy/fields.py
+++ marshmallow_sqlalchemy/fields.py
@@ -91,7 +91,8 @@ class Related(fields.Field):
             else:
                 # Use a faster path if the related key is the primary key.
                 for prop in self.related_model.__mapper__.iterate_properties:
-                    print("prop is %s"% prop)
+                    if hasattr(prop, 'direction') :                        
+                        print("prop is %s"% prop)
                     #print("in columns - %s" % self.related_keys)
                 result = query.get([
                     value.get(prop.key) for prop in self.related_keys
